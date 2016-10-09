fd6f53a59e3bd58dead958658620e81b0c948684
Stephen Washington <stephen@stephenwashington.net>
2016-10-09 17:52:29 -0400

    finished test for valid, invalid special characters

--- check_rpn_utilities.c
+++ check_rpn_utilities.c
@@ -15,8 +15,12 @@ START_TEST (test_identify_type_A){
     ck_assert_int_eq(identify_type('A'), -1);
 } END_TEST
 
-START_TEST (test_identify_special_char){
+START_TEST (test_identify_valid_special_char){
     ck_assert_int_eq(identify_type('/'), 1);
+} END_TEST
+
+START_TEST (test_identify_invalid_special_char){
+    ck_assert_int_eq(identify_type('{'), -1);
 } END_TEST
 
 Suite * utility_suite(void){
@@ -29,7 +33,8 @@ Suite * utility_suite(void){
     tcase_add_test(tc_core, test_identify_type_a);
     tcase_add_test(tc_core, test_identify_type_b);
     tcase_add_test(tc_core, test_identify_type_A);
-    tcase_add_test(tc_core, test_identify_special_char);
+    tcase_add_test(tc_core, test_identify_valid_special_char);
+    tcase_add_test(tc_core, test_identify_invalid_special_char);
     suite_add_tcase(s, tc_core);
 
     return s;
