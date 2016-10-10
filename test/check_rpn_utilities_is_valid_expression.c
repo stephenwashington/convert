6fb5108cb6ee8f17953f6ee944ca3b5c086f814c
Stephen Washington <stephen@stephenwashington.net>
2016-10-10 18:59:44 -0400

    finished first multi-character is_valid_expression test

--- test/check_rpn_utilities_is_valid_expression.c
+++ test/check_rpn_utilities_is_valid_expression.c
@@ -4,14 +4,19 @@
 #include "rpn_utilities.h"
 #include "rpn_unit_tests.h"
 
-START_TEST(test_is_valid_char_a){
+START_TEST(test_is_valid_expression_a){
     uint8_t exp[1] = {'a'};
     ck_assert_int_eq(is_valid_expression(exp), true);
 } END_TEST
 
-START_TEST(test_is_valid_char_A){
+START_TEST(test_is_valid_expression_A){
     uint8_t exp[1] = {'A'};
     ck_assert_int_eq(is_valid_expression(exp), false);
+} END_TEST
+
+START_TEST(test_is_valid_expression_aplusb){
+    uint8_t exp[3] = {'a', '+', 'b'};
+    ck_assert_int_eq(is_valid_expression(exp), true);
 } END_TEST
 
 Suite * make_is_valid_expression_suite(void){
@@ -21,8 +26,9 @@ Suite * make_is_valid_expression_suite(void){
     s = suite_create("is_valid_expression");
     tc_core = tcase_create("Core");
 
-    tcase_add_test(tc_core, test_is_valid_char_a);
-    tcase_add_test(tc_core, test_is_valid_char_A);
+    tcase_add_test(tc_core, test_is_valid_expression_a);
+    tcase_add_test(tc_core, test_is_valid_expression_A);
+    tcase_add_test(tc_core, test_is_valid_expression_aplusb);
     suite_add_tcase(s, tc_core);
 
     return s;
