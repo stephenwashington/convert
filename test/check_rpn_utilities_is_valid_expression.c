b1d4b9534e32f34c64672c41b11cf80f6857e571
Stephen Washington <stephen@stephenwashington.net>
2016-10-10 18:49:25 -0400

    finished uppercase test for is_valid_expression

--- test/check_rpn_utilities_is_valid_expression.c
+++ test/check_rpn_utilities_is_valid_expression.c
@@ -9,6 +9,11 @@ START_TEST(test_is_valid_char_a){
     ck_assert_int_eq(is_valid_expression(exp), true);
 } END_TEST
 
+START_TEST(test_is_valid_char_A){
+    uint8_t exp[1] = {'A'};
+    ck_assert_int_eq(is_valid_expression(exp), false);
+} END_TEST
+
 Suite * make_is_valid_expression_suite(void){
     Suite *s;
     TCase *tc_core;
@@ -17,6 +22,7 @@ Suite * make_is_valid_expression_suite(void){
     tc_core = tcase_create("Core");
 
     tcase_add_test(tc_core, test_is_valid_char_a);
+    tcase_add_test(tc_core, test_is_valid_char_A);
     suite_add_tcase(s, tc_core);
 
     return s;
