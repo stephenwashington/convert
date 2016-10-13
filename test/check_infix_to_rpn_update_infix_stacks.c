4cf530e2661561fb47cee2e1e5c840f49ac6fcb2
Stephen Washington <stephen@stephenwashington.net>
2016-10-13 16:31:15 -0400

    finished test for (a+g)*(((b-a)+c)^(c+(e*(d^f))))

--- test/check_infix_to_rpn_update_infix_stacks.c
+++ test/check_infix_to_rpn_update_infix_stacks.c
@@ -128,6 +128,26 @@ START_TEST(test_update_infix_stacks_expr_with_parentheses){
     free(final_expr);
 } END_TEST
 
+START_TEST(test_update_infix_stacks_complex_expression){
+    struct stack variable_stack = { .content = {0}, .length = 0};
+    struct stack symbol_stack = { .content = {0}, .length = 0};
+    const char input[] = "(a+g)*(((b-a)+c)^(c+(e*(d^f))))";
+    for (uint16_t i = 0; i < strlen(input); i++){
+        update_infix_stacks(&variable_stack, &symbol_stack, input[i]);
+    }
+    char *final_expr = calloc(variable_stack.length + symbol_stack.length + 1,\
+                              sizeof(char));
+                              
+    for (int16_t i = symbol_stack.length-1; i >= 0; i--){
+        variable_stack.content[variable_stack.length] = symbol_stack.content[i];
+        variable_stack.length++;
+    }
+    
+    ck_assert_str_eq((char*)variable_stack.content, "ag+ba-c+cedf^*+^*");
+    ck_assert_int_eq(variable_stack.length, 17);
+    free(final_expr);
+} END_TEST
+
 
 Suite * make_update_infix_stacks_suite(void){
     Suite *s;
@@ -144,6 +164,7 @@ Suite * make_update_infix_stacks_suite(void){
                    test_update_infix_stacks_a_plus_b_minus_c_with_paren);
     tcase_add_test(tc_core, test_update_infix_stacks_obey_order_of_operations);
     tcase_add_test(tc_core, test_update_infix_stacks_expr_with_parentheses);
+    tcase_add_test(tc_core, test_update_infix_stacks_complex_expression);
     suite_add_tcase(s, tc_core);
 
     return s;
