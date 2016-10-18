e4e9262f2f0e4170dfb55e0ccae1c8218776daa7
Stephen Washington <stephen@stephenwashington.net>
2016-10-17 22:17:51 -0400

    fixed a couple bugs related to over/underflowing user input

--- src/main.c
+++ src/main.c
@@ -8,25 +8,40 @@
 
 int main(void){
     char expr[500];
-    printf("\n========== RPN To Infix Converter ==========\n\n");
-    printf("Please input your expression to convert (max 500 characters): ");
+    printf("\n========== RPN To Infix Converter ==========\n");
+    printf("Valid characters: lowercase alphabet (a-z), +, -, *, /, ^, (, and )\n");
+    printf("Please input your expression to convert: ");
     
     if (fgets(expr, 500, stdin) == NULL){
         fprintf(stderr, "Couldn't get expression\n");
         exit(EXIT_FAILURE);
     }
-    expr[strlen(expr) - 1] = '\0';
+
+    if (expr[strlen(expr) - 1] != '\n'){
+        fprintf(stderr, "Input too long (max 500 characters)\n");
+        exit(EXIT_FAILURE);
+    } else if (expr[0] == 0 || expr[0] == 10){
+        fprintf(stderr, "Empty input\n");
+        exit(EXIT_FAILURE);
+    }
+    
+    if (expr[strlen(expr) - 1] != '\0'){
+        expr[strlen(expr) - 1] = '\0';
+    }
     
     printf("[1]: Convert from Reverse Polish Notation to Infix Notation\n");
     printf("[2]: Convert from Infix Notation to Reverse Polish Notation\n\n");
     printf("Your selection: ");
+
     uint8_t option = getchar();
     
     while ((option != '1') && (option != '2')){
-        printf("Invalid option. Please select either '1' or '2': ");
+        if (option != '\n'){
+            printf("Invalid option. Please select either '1' or '2': ");
+        }
         option = getchar();
-        printf("\n");
     }
+    printf("\n");
     
     if (option == '1'){
         printf("RPN: %s\n",expr);
